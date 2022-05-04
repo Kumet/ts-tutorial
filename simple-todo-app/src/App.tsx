fb82e12bc34bf99e2e606f1296bef33ad7d392a5
m.kumeta <m.kumeta@nakamu-farm.com>
2022-05-04 14:17:35 +0900

    create simple-todo-app with react

---
+++ simple-todo-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import {Todo} from "./todo.model";
+
+const App: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([])
+
+    const todoAddHandler = (text: string) => {
+        setTodos(prevState => [...prevState, {id: Math.random().toString(), text: text}])
+    }
+
+    const todoRemoveHandler = (todoId: string) => {
+        setTodos(prevState => prevState.filter(todo => todo.id !== todoId))
+    }
+
+    return (
+        <div className="App">
+            <NewTodo onAddTodo={todoAddHandler}/>
+            <TodoList items={todos} onRemoveTodo={todoRemoveHandler}/>
+        </div>
+    )
+}
+export default App;
